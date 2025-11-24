506b7ef83c9e0a69e5fb3d8dba6ac669910a200d
Riya Agrahari <agrahaririya5892@gmail.com>
2025-11-24 15:09:19 +0530

    server added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 import http from 'http';
 const server=http.createServer((req,res)=>{
   if(req.url==="/"){
-    res.write("HHome page")
+    res.write("Home page")
+  }else if(req.url==="/about"){
+    res.write("About Page")
+  }else{
+    res.write("Error: 404 page not found")
   }
+  res.end();
+})
+
+server.listen(3001,()=>{
+  console.log("server is working on port 8001")
 })
